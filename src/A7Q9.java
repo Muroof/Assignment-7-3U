335de848c6c91a7238a696214de491a68ee81aa8
ranam3235 <ranam3235@HRH-IC0027321.wrdsb.ca>
2017-05-18 09:35:23 -0400

    Assignment 9 Complete, commenting needed

--- src/A7Q9.java
+++ src/A7Q9.java
@@ -14,7 +14,32 @@ public class A7Q9 {
      */
     public static void allDigitsOdd(int number) {
         
+        int check = 0;
+
+        while (number > 10) {
+
+            int even = number % 2;
+
+            if (even == 0) {
+                System.out.println("False");
+                check = 1;
+                break;
+            }
+            number = number / 10;
+
+
+        }
+        if (check == 0) {
+            System.out.println("True");
+        }
         
-        
+
+
+
+
+    }
+    
+    public static void main(String[] args) {
+        allDigitsOdd(395);
     }
 }
